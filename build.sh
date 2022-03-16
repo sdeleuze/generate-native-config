ed16511d4e1dcb08684bc5faca52c22b0002aef6
Sébastien Deleuze <sdeleuze@vmware.com>
2022-03-16 16:30:19 +0100

    Use native build tools

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 mvn clean compile assembly:single
 java -jar target/generate-native-config-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-native-image -H:-InlineBeforeAnalysis --no-fallback -H:Log=registerResource:3 -H:Class=com.example.generate.NativeApplication -cp target/classes -H:Name=nativeapp
-./nativeapp
+mvn -Pnative package
+./target/nativeapp
